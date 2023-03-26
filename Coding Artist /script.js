9ffdb1557636ed024533798ed74d4d78b68cdcda
tiagodacosta1 <tiagocosta4823@gmail.com>
2023-03-26 18:48:06 +0100

    renamaed the file

--- Coding Artist /script.js
+++
@@ -1,9 +0,0 @@
-const date = new Date()
-
-
-let day = date.getDate()
-let month = (date.getMonth() + 1).toString().padStart(2, '0'); // pads the month with a leading 0 if it's a single digit month
-let year = date.getFullYear()
-
-let fullDate = `${day}-${month}-${year}`
-document.getElementById("date").innerHTML = fullDate;
