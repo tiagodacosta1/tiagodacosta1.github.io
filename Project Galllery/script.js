c1ebc69617c80740a7080bb346569c5390c14fba
tiagodacosta1 <tiagocosta4823@gmail.com>
2023-03-27 02:03:30 +0100

    commit

--- Project Galllery/script.js
+++ Project Galllery/script.js
@@ -3,12 +3,12 @@ var readMoreBtns = document.querySelectorAll('.btn-read-more');
 readMoreBtns.forEach(function(btn) {
   btn.addEventListener('click', function() {
     var description = this.parentNode.querySelector('.project-description');
-    if (description.style.display === 'none') {
+    if (description.style.display === 'none' || description.style.display === '') {
       description.style.display = 'block';
-      this.innerHTML = 'Read Less';
+      this.innerHTML = 'Read less';
     } else {
       description.style.display = 'none';
-      this.innerHTML = 'Read More';
+      this.innerHTML = 'Read more';
     }
   });
 });
